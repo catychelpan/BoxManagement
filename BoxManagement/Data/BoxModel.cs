9becf2614cce5e85808625aa388723e45a966389
Kateryna Chelpan <catychelpan@gmail.com>
2023-11-28 18:43:36 +0200

    Add project files.

---
+++ BoxManagement/Data/BoxModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace BoxManagement.Data
+{
+	public class BoxModel
+	{
+        public BoxModel()
+        {
+            
+        }
+        public BoxModel(int id, string name)
+        {
+            this.Id = id;
+            this.Name = name;
+        }
+
+        [Required]
+        public int Id { get; set; }
+
+		[Required]
+		[StringLength(100)]
+        [NotNull]
+		public string Name { get; set; }
+
+	}
+}
---
+++ BoxManagement/Data/ItemModel.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace BoxManagement.Data
+{
+	public class ItemModel
+	{
+
+        public ItemModel()
+        {
+            
+        }
+        public ItemModel(string name,int boxId, string igm)
+        {
+            this.Name = name;
+            this.BoxId = boxId;
+            this.ImgURL = igm;
+
+        }
+
+		[Required]
+        [NotNull]
+		public string? Name { get; set; }
+
+		[Required]
+		public int BoxId { get; set; }
+
+        [Required]
+        [Url]
+        [NotNull]
+        public string? ImgURL { get; set; }
+    }
+}
